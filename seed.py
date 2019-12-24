67f39f85686799bf0d586c9c4e7d8233f6319e2a
Adam Barborik <52447925+Barbo24@users.noreply.github.com>
2019-12-24 21:06:41 +0100

    Add files via upload

--- seed.py
+++ seed.py
@@ -26,12 +26,8 @@ def seed():
     with open("saves/chunky.json", "r") as chunk:
         chunky_data = chunk.read()
         chunky = json.loads(chunky_data)       
-    if(chunkx < 0):
-        chunkx *= -1
-        chunkx += 0.5
-    if(chunky < 0):
-        chunky *= -1
-        chunky += 0.5
     userseed = 69
-    seed = int((chunkx * userseed) + (chunky * userseed) + userseed)
+    temp = '{}{}{}'.format(chunkx, chunky, userseed)
+    temp_hash = hash(temp)
+    seed = temp_hash & 0xFFFFFFFF
     return seed
